6f69a8d18020662666b4f7fdd2e21d47c7f4a52a
quidsup <ooop@btinternet.com>
2016-04-24 19:22:47 +0100

    Update flashless.sh

--- flashless.sh
+++ flashless.sh
@@ -8,10 +8,10 @@
 if [ "$(command -v apt-get)" ]; then        #Is this an Ubuntu / Debian based distro?
   if [[ $(pgrep kwin) != "" ]]; then        #Different set of packages are needed for KDE based systems
     echo "Installing Restricted Extras for KDE based system"
-    sudo apt-get install lame unrar gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-fluendo-mp3 libdvdread4 libk3b6-extracodecs nepomuk-core-ffmpegextractor oxideqt-codecs-extra libavcodec-extra libadcodec-ffmpeg-extra56 libk3b6-extracodecs
+    sudo apt-get install synaptic oxygen-cursor-theme oxygen-cursor-theme-extra lame unrar gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-fluendo-mp3 libdvdread4 libk3b6-extracodecs  oxideqt-codecs-extra libavcodec-extra libavcodec-ffmpeg-extra56 libk3b6-extracodecs
   else
     echo "Installing Restricted Extras"
-    sudo apt-get install lame unrar gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-fluendo-mp3 libdvdread4 libk3b6-extracodecs nepomuk-core-ffmpegextractor oxideqt-codecs-extra libavcodec-extra libadcodec-ffmpeg-extra56
+    sudo apt-get install synaptic oxygen-cursor-theme oxygen-cursor-theme-extra lame unrar gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-fluendo-mp3 libdvdread4 libk3b6-extracodecs  oxideqt-codecs-extra libavcodec-extra libavcodec-ffmpeg-extra56
   fi
 else                                        #Throw error for non Deb systems
   echo "Error: Flashless Extras is only written for Debian based systems"
